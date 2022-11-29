c1b3cd4b82443cbc3eadfbeb560bf625e6300588
Vincent Nebo <nebov3@gmail.com>
2022-11-29 09:47:04 +0100

    SEO changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,8 @@ import Image from "next/image";
 import styles from "styles/Home.module.css";
 import Script from "next/script";
 import PageLoader from "components/utility/PageLoader";
+import { HelmetProvider, Helmet } from "react-helmet-async";
+import nextConfig from "next.config";
 
 const Home: NextPage = () => {
   return (
@@ -12,10 +14,32 @@ const Home: NextPage = () => {
       <Head>
         <title>Utility NextJS Website</title>
         <meta name="description" content="Created by Vincent Nebo" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon-dark.png" />
       </Head>
+      {/* SEO OPTIMIZATION WITH REACT HELMET ASYNC */}
+      <HelmetProvider>
+        <Helmet>
+          <title>Utility NextJS Website</title>
+          <link rel="canonical" href="" />
+          {/* special metatag */}
+          <link rel="canonical" href="" />
+          <meta property="og:locale" content="en_US" />
+          <meta property="og:type" content="website" />
+          <meta
+            property="og:title"
+            content="Utility NextJS Website by Vincent Nebo"
+          />
+          <meta
+            property="og:description"
+            content="Multi-purpose Utility web application created with React, NextJs and ExpressJs"
+          />
+          <meta property="og:url" content={nextConfig?.env?.appUrl} />
+          <meta property="og:image" content="/favicon-dark.png" />
+          {/* special metatag */}
+        </Helmet>
+      </HelmetProvider>
+      {/* SEO OPTIMIZATION WITH REACT HELMET ASYNC */}
       <LandingPage />
-      {/* <PageLoader /> */}
     </div>
   );
 };
